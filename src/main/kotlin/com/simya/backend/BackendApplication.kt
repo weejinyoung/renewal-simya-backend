07172272c9d5e67891350a1150a09120d11ea5b4
weejinyoung <a8118199@gmail.com>
2023-06-03 19:20:47 +0900

    env: add properties path

--- src/main/kotlin/com/simya/backend/BackendApplication.kt
+++ src/main/kotlin/com/simya/backend/BackendApplication.kt
@@ -2,8 +2,10 @@ package com.simya.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("classpath:secure.properties")
 class BackendApplication
 
 fun main(args: Array<String>) {
