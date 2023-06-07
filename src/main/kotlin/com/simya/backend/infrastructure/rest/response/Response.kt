94cf2135661a40ae1968bca56539ce80aeca69bd
weejinyoung <a8118199@gmail.com>
2023-06-07 12:41:56 +0900

    feat: custom response

---
+++ src/main/kotlin/com/simya/backend/infrastructure/rest/response/Response.kt
@@ -0,0 +1,23 @@
+package com.simya.backend.infrastructure.rest.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import io.swagger.v3.oas.annotations.media.Schema
+
+
+// TODO CODE 필드추가
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(*["message", "data"])
+data class Response (
+    val message: String,
+    val data: Any?
+) {
+    companion object {
+        fun success(message: String): Response =
+            Response(message, null)
+        fun success(message: String, data: Any): Response =
+            Response(message, data)
+        fun failure(message: String): Response =
+            Response(message, null)
+    }
+}
